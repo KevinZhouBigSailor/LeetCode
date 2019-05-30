b24151f993d16d7b2838f04e5e18f12bb9f291f1
z.zhou <z.zhou@alibaba-inc.com>
2019-05-31 06:21:33 +0800

    update

--- src/Robinhood/ReturnOnInvestments.java
+++ src/Robinhood/ReturnOnInvestments.java
@@ -35,8 +35,75 @@ public class ReturnOnInvestments {
         return output;
     }
 
-    //@Test
-    public static void main(String[] args) {
+    public int tradeCounts(List<Stock> sells, List<Stock> buys) {
+        int count = 0;
+        HashMap<String, PriorityQueue<Stock>> maps = new HashMap<>();
+        for (Stock sell : sells) {
+            if (!maps.containsKey(sell.name)) {
+                maps.put(sell.name, new PriorityQueue<>());
+            }
+            maps.get(sell.name).add(sell);
+        }
+        for (Stock buy : buys) {
+            while (maps.get(buy.name).size() > 0 && maps.get(buy.name).peek().price < buy.price && buy.count > 0) {
+                Stock sell = maps.get(buy.name).poll();
+                if (sell.count > buy.count) {
+                    sell.count -= buy.count;
+                    count++;
+                    maps.get(buy.name).add(sell);
+                    break;
+                } else {
+                    buy.count -= sell.count;
+                    count++;
+                }
+                //if (maps.get(buy.name).size() == 0) break;
+            }
+        }
+        return count;
+    }
+
+    class Stock implements Comparable<Stock> {
+        String name;
+        int count;
+        int price;
+
+        Stock(String name, int count, int price) {
+            this.name = name;
+            this.count = count;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Stock o) {
+            if (this.price == o.price) {
+                return 0;
+            } else if (this.price < o.price) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    @Test
+    public void test() {
+        Stock sell1 = new Stock("AAPL", 1, 1);
+        Stock sell2 = new Stock("AAPL", 1, 3);
+        Stock buy1 = new Stock("AAPL", 2, 2);
+        //Stock buy2 = new Stock("AAPL", 1, 2);
+
+        List<Stock> sells = new ArrayList<>();
+        sells.add(sell1);
+        sells.add(sell2);
+        List<Stock> buys = new ArrayList<>();
+        buys.add(buy1);
+        //buys.add(buy2);
+
+        System.out.println(tradeCounts(sells, buys));
+    }
+
+    @Test
+    public void maintest() {
         ReturnOnInvestments investments = new ReturnOnInvestments();
         List<String> inputOnes = new ArrayList<>();
         inputOnes.add("4 AAPL valued 100 each");
