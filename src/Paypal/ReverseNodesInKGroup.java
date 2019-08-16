59792cd68ab0d607f55e4680d31b533354040eb5
z.zhou <z.zhou@alibaba-inc.com>
2019-08-17 00:58:13 +0800

    add

---
+++ src/Paypal/ReverseNodesInKGroup.java
@@ -0,0 +1,53 @@
+package Paypal;/*
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+ */
+
+public class ReverseNodesInKGroup {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+
+        while (cur != null && count != k) {
+            cur = cur.next;
+            count++;
+        }
+
+        if (count == k) { // if k+1 node is found
+            cur = reverseKGroup(cur, k); // reverse list with k+1 node as head
+            // head - head-pointer to direct part,
+            // curr - head-pointer to reversed part;
+            while (count-- > 0) { // reverse current k-group:
+                ListNode tmp = head.next; // tmp - next head in direct part
+                head.next = cur; // preappending "direct" head to the reversed list
+                cur = head; // move head of reversed part to a new node
+                head = tmp; // move "direct" head to the next node in direct part
+            }
+            head = cur;
+        }
+
+        return head;
+    }
+}
