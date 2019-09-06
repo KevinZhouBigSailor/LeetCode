6630d2f1570bae8b72d698b26d67e3d0df246f68
z.zhou <z.zhou@alibaba-inc.com>
2019-09-07 00:10:46 +0800

    add

--- src/sumoLogic/PermutationInString.java
+++ src/sumoLogic/PermutationInString.java
@@ -1,5 +1,10 @@
 package sumoLogic;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length())
@@ -32,30 +37,39 @@ public class PermutationInString {
         return count == 26;
     }
 
-    public class Solution {
-        public boolean checkInclusion(String s1, String s2) {
-            if (s1.length() > s2.length())
+
+    public List<String> checkInclusion2(String s1, String s2) {
+        List<String> results = new ArrayList<>();
+        if (s1.length() > s2.length())
+            return null;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                results.add(s2.substring(i, i + s1.length()));
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+            s2map[s2.charAt(i) - 'a']--;
+        }
+        return results;
+    }
+
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
                 return false;
-            int[] s1map = new int[26];
-            int[] s2map = new int[26];
-            for (int i = 0; i < s1.length(); i++) {
-                s1map[s1.charAt(i) - 'a']++;
-                s2map[s2.charAt(i) - 'a']++;
-            }
-            for (int i = 0; i < s2.length() - s1.length(); i++) {
-                if (matches(s1map, s2map))
-                    return true;
-                s2map[s2.charAt(i + s1.length()) - 'a']++;
-                s2map[s2.charAt(i) - 'a']--;
-            }
-            return matches(s1map, s2map);
         }
-        public boolean matches(int[] s1map, int[] s2map) {
-            for (int i = 0; i < 26; i++) {
-                if (s1map[i] != s2map[i])
-                    return false;
-            }
-            return true;
-        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        PermutationInString s = new PermutationInString();
+        String s1 = "aba";
+        String s2 = "abaaabaca";
+        System.out.println(s.checkInclusion2(s1, s2));
     }
 }
