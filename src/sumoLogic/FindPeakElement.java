ae9a2aca23c05796d805f34dea2f9327cd72485d
z.zhou <z.zhou@alibaba-inc.com>
2019-09-25 07:38:22 +0800

    update

--- src/sumoLogic/FindPeakElement.java
+++ src/sumoLogic/FindPeakElement.java
@@ -11,9 +11,15 @@ public class FindPeakElement {
     public int search(int[] nums, int l, int r) {
         if (l == r)
             return l;
-        int mid = r + (l - r) / 2;
+        int mid = l + ((r - l) >> 1); //(l + r) >> 1;
         if (nums[mid] > nums[mid + 1])
             return search(nums, l, mid);
         return search(nums, mid + 1, r);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        FindPeakElement ins = new FindPeakElement();
+        ins.findPeakElement(nums);
     }
 }
