dd97a46e90a2c62345f3f84dd74331d3888ef99c
z.zhou <z.zhou@alibaba-inc.com>
2019-09-30 00:51:18 +0800

    update

--- src/sumoLogic/NestedListWeightSumII.java
+++ src/sumoLogic/NestedListWeightSumII.java
@@ -22,51 +22,21 @@ public class NestedListWeightSumII {
     }
 
     public int depthSumInverse2(List<NestedInteger> nestedList) {
-        if (nestedList == null) return 0;
-        Queue<NestedInteger> queue = new LinkedList<NestedInteger>();
-        int prev = 0;
-        int total = 0;
-        for (NestedInteger next : nestedList) {
-            queue.offer(next);
-        }
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            int levelSum = 0;
-            for (int i = 0; i < size; i++) {
-                NestedInteger current = queue.poll();
-                if (current.isInteger()) {
-                    levelSum += current.getInteger();
-                }
-                List<NestedInteger> nextList = current.getList();
-                if (nextList != null) {
-                    for (NestedInteger next : nextList) {
-                        queue.offer(next);
-                    }
-                }
-            }
-            prev += levelSum;
-            total += prev;
-        }
-        return total;
-    }
-
-    public int depthSumInverse3(List<NestedInteger> nestedList) {
         int prevSum = 0, totalSum = 0;
-        Deque<NestedInteger> queue = new ArrayDeque();
+        Queue<NestedInteger> queue = new ArrayDeque();
         for (NestedInteger ni : nestedList) {
-            queue.offerLast(ni);
+            queue.offer(ni);
         }
 
         while (!queue.isEmpty()) {
             int size = queue.size(), levelSum = 0;
             for (int i = 0; i < size; i++) {
-                NestedInteger current = queue.pollFirst();
+                NestedInteger current = queue.poll();
                 if (current.isInteger()) {
                     levelSum += current.getInteger();
                 } else {
                     for (NestedInteger ni: current.getList()) {
-                        queue.offerLast(ni);
+                        queue.offer(ni);
                     }
                 }
             }
