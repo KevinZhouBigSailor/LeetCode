31d0f6b2a072e3a84d98dcdd3693346466f0c428
z.zhou <z.zhou@alibaba-inc.com>
2019-11-01 05:00:51 +0800

    add

---
+++ src/LinkedIn/FactorCombinations.java
@@ -0,0 +1,31 @@
+package LinkedIn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zzhou on 1/3/2018.
+ */
+public class FactorCombinations {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res, new ArrayList<>(), n, 2);
+        return res;
+    }
+
+    public void helper(List<List<Integer>> result, List<Integer> item, int n, int start) {
+        if (n <= 1) {
+            if (item.size() > 1) {
+                result.add(new ArrayList<Integer>(item));
+            }
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (n % i == 0) {
+                item.add(i);
+                helper(result, item, n / i, i);
+                item.remove(item.size() - 1);
+            }
+        }
+    }
+}
---
+++ src/LinkedIn/MaximumDepthofBinaryTree.java
@@ -0,0 +1,10 @@
+package LinkedIn;
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
