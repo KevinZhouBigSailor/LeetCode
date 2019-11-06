0fd0666cc93a04ff8ddf934db6214f4861ea1c06
z.zhou <z.zhou@alibaba-inc.com>
2019-11-07 02:30:12 +0800

    add

---
+++ src/Twitter/ReachingPoints.java
@@ -0,0 +1,54 @@
+package Twitter;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReachingPoints {
+
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (tx == ty) break;
+            if (tx > ty) {
+                if (ty > sy) tx %= ty;
+                else return (tx - sx) % ty == 0;
+            } else {
+                if (tx > sx) ty %= tx;
+                else return (ty - sy) % tx == 0;
+            }
+        }
+        return (tx == sx && ty == sy);
+    }
+
+    public boolean reachingPoints2(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (sx == tx && sy == ty)
+                return true;
+            if (tx > ty) tx -= ty;
+            else ty -= tx;
+        }
+        return false;
+    }
+
+    class Solution {
+        Set<Point> seen;
+        int tx, ty;
+
+        public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+            seen = new HashSet();
+            this.tx = tx;
+            this.ty = ty;
+            search(new Point(sx, sy));
+            return seen.contains(new Point(tx, ty));
+        }
+
+        public void search(Point P) {
+            if (seen.contains(P)) return;
+            if (P.x > tx || P.y > ty) return;
+            seen.add(P);
+            search(new Point(P.x + P.y, P.y));
+            search(new Point(P.x, P.x + P.y));
+        }
+    }
+
+}
