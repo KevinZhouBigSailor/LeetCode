97ab17249d863eb0c37ed77cb85a563f8239877e
Kevin Zhou <kevinzhou1990@hotmail.com>
2019-11-18 16:45:31 -0800

    add

---
+++ src/Twitter/CombinationSum_39.java
@@ -0,0 +1,27 @@
+package Twitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zzhou on 1/31/2018.
+ */
+public class CombinationSum_39 {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtracking(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtracking(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0) return;
+        if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
+            for (int i = start; i < nums.length; i++) {
+                tempList.add(nums[i]);
+                backtracking(list, tempList, nums, remain - nums[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
