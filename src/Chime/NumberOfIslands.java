bfa4f8653ef8310e1b519ef0da9260499a9666fa
Kevin Zhou <kevinzhou1990@hotmail.com>
2020-05-26 10:35:27 -0700

    update

--- src/Chime/NumberOfIslands.java
+++ src/Chime/NumberOfIslands.java
@@ -1,5 +1,8 @@
 package Chime;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumberOfIslands {
     private int n;
     private int m;
@@ -11,7 +14,7 @@ public class NumberOfIslands {
         m = grid[0].length;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     DFSMarking(grid, i, j);
                     ++count;
                 }
@@ -28,5 +31,51 @@ public class NumberOfIslands {
         DFSMarking(grid, i, j - 1);
         DFSMarking(grid, i + 1, j);
         DFSMarking(grid, i, j + 1);
+    }
+
+    class Solution {
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+
+            int nr = grid.length;
+            int nc = grid[0].length;
+            int num_islands = 0;
+
+            for (int r = 0; r < nr; ++r) {
+                for (int c = 0; c < nc; ++c) {
+                    if (grid[r][c] == '1') {
+                        ++num_islands;
+                        grid[r][c] = '0'; // mark as visited
+                        Queue<Integer> neighbors = new LinkedList<>();
+                        neighbors.add(r * nc + c);
+                        while (!neighbors.isEmpty()) {
+                            int id = neighbors.remove();
+                            int row = id / nc;
+                            int col = id % nc;
+                            if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                                neighbors.add((row - 1) * nc + col);
+                                grid[row - 1][col] = '0';
+                            }
+                            if (row + 1 < nr && grid[row + 1][col] == '1') {
+                                neighbors.add((row + 1) * nc + col);
+                                grid[row + 1][col] = '0';
+                            }
+                            if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                                neighbors.add(row * nc + col - 1);
+                                grid[row][col - 1] = '0';
+                            }
+                            if (col + 1 < nc && grid[row][col + 1] == '1') {
+                                neighbors.add(row * nc + col + 1);
+                                grid[row][col + 1] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+
+            return num_islands;
+        }
     }
 }
