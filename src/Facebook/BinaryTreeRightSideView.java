87b0816fe7fee6b127a3c611530725a6f483b033
Kevin Zhou <kevinzhou1990@hotmail.com>
2020-05-31 17:36:28 -0700

    add

---
+++ src/Facebook/BinaryTreeRightSideView.java
@@ -0,0 +1,25 @@
+package Facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zzhou on 2/7/2018.
+ */
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    public void rightView(TreeNode curr, List<Integer> result, int curDepth) {
+        if (curr == null) return;
+        if (curDepth == result.size()) {
+            result.add(curr.val);
+        }
+
+        rightView(curr.right, result, curDepth + 1);
+        rightView(curr.left, result, curDepth + 1);
+    }
+}
---
+++ src/Facebook/TreeNode.java
@@ -0,0 +1,11 @@
+package Facebook;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
