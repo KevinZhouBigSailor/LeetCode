f567462619a2fca37194cb1220c2bf587c93fa97
KevinZhouBigSailor <kevinzhou1990@hotmail.com>
2022-01-07 14:18:01 -0800

    add

--- src/Google/GuessTheWord_843.java
+++ src/Google/GuessTheWord_843.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+package Google;
+
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/guess-the-word/discuss/133862/Random-Guess-and-Minimax-Guess-with-Comparison
  */
 
 public class GuessTheWord_843 {
-    //int[][] H;
-
     public void findSecretWord(String[] wordlist, Master master) {
         for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
             HashMap<String, Integer> count = new HashMap<>();
@@ -64,67 +62,6 @@ public class GuessTheWord_843 {
                 matches ++;
         return matches;
     }
-
-    /*int N = wordlist.length;
-        H = new int[N][N];
-        for (int i = 0; i < N; ++i)
-            for (int j = i; j < N; ++j) {
-                int match = 0;
-                for (int k = 0; k < 6; ++k)
-                    if (wordlist[i].charAt(k) == wordlist[j].charAt(k))
-                        match++;
-                H[i][j] = H[j][i] = match;
-            }
-
-        List<Integer> possible = new ArrayList();
-        HashSet<Integer> path = new HashSet();
-        for (int i = 0; i < N; ++i) possible.add(i);
-
-        while (!possible.isEmpty()) {
-            int guess = solve(possible, path);
-            int matches = master.guess(wordlist[guess]);
-            if (matches == wordlist[0].length()) return;
-            List<Integer> possible2 = new ArrayList();
-            for (Integer j : possible) if (H[guess][j] == matches) possible2.add(j);
-            possible = possible2;
-            path.add(guess);
-        }*/
-
-    /**
-     * Get most common word index from the remaining indexes
-     *
-     * @param possible
-     * @param path
-     * @return
-     */
-    /*public int solve(List<Integer> possible, HashSet<Integer> path) {
-        if (possible.size() <= 2) return possible.get(0);
-        List<Integer> ansgrp = possible;
-        int ansguess = -1;
-
-        for (int guess = 0; guess < H.length; ++guess) {
-            if (!path.contains(guess)) {
-                ArrayList<Integer>[] groups = new ArrayList[7];
-                for (int i = 0; i < 7; ++i) groups[i] = new ArrayList<Integer>();
-                for (Integer j : possible)
-                    if (j != guess) {
-                        groups[H[guess][j]].add(j);
-                    }
-
-                ArrayList<Integer> maxgroup = groups[0];
-                for (int i = 0; i < 7; ++i)
-                    if (groups[i].size() > maxgroup.size())
-                        maxgroup = groups[i];
-
-                if (maxgroup.size() < ansgrp.size()) {
-                    ansgrp = maxgroup;
-                    ansguess = guess;
-                }
-            }
-        }
-
-        return ansguess;
-    }*/
 }
 
 interface Master {
