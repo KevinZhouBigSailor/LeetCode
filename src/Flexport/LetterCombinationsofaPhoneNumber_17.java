0eebf8e917712630c60a30c11ec5548fa15fe07d
KevinZhouBigSailor <kevinzhou1990@hotmail.com>
2022-01-09 21:36:48 -0800

    update

--- src/Flexport/LetterCombinationsofaPhoneNumber_17.java
+++ src/Flexport/LetterCombinationsofaPhoneNumber_17.java
@@ -1,7 +1,6 @@
 package Flexport;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LetterCombinationsofaPhoneNumber_17 {
     public List<String> letterCombinations(String digits) {
@@ -19,5 +18,42 @@ public class LetterCombinationsofaPhoneNumber_17 {
             }
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = Map.of(
+                "2", "abc",
+                "3", "def",
+                "4", "ghi",
+                "5", "jkl",
+                "6", "mno",
+                "7", "pqrs",
+                "8", "tuv",
+                "9", "wxyz",
+                "23", "qcd"
+        );
+        List<String> result = new ArrayList<>();
+        dfs(result, map, "234", 0, new StringBuilder());
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+
+    public static void dfs(List<String> result, Map<String, String> map, String digits, int index, StringBuilder
+            current) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+        } else {
+            for (int j = index + 1; j <= digits.length(); j++) {
+                String key = digits.substring(index, j);
+                String value = map.get(key);
+                if (value == null) {
+                    return;
+                }
+                for (int i = 0; i < value.length(); i++) {
+                    current.append(value.charAt(i));
+                    dfs(result, map, digits, j, current);
+                    current.deleteCharAt(current.length() - 1);
+                }
+            }
+        }
     }
 }
