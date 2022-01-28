45a0a74f7d1e0054c17cfe2d8d7805db7b70c99f
KevinZhouBigSailor <kevinzhou1990@hotmail.com>
2022-01-28 09:44:15 -0800

    add'

---
+++ src/Facebook/RandomPickWithWeight_528.java
@@ -0,0 +1,32 @@
+package Facebook;
+
+import java.util.Random;
+
+public class RandomPickWithWeight_528 {
+    Random random;
+    int[] wSums;
+
+    public RandomPickWithWeight_528(int[] w) {
+        this.random = new Random();
+        for (int i = 1; i < w.length; i++) {
+            w[i] += w[i - 1];
+        }
+        this.wSums = w;
+    }
+
+    public int pickIndex() {
+        int len = wSums.length;
+        int idx = random.nextInt(wSums[len - 1]) + 1;
+        int left = 0, right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (wSums[mid] == idx)
+                return mid;
+            else if (wSums[mid] < idx)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
