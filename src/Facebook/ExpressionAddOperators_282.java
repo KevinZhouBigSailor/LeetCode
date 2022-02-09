47ac0430a8e6453b000021cc8059d12f411f85a5
KevinZhouBigSailor <kevinzhou1990@hotmail.com>
2022-02-09 10:23:32 -0800

    add

---
+++ src/Facebook/ExpressionAddOperators_282.java
@@ -0,0 +1,34 @@
+package Facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionAddOperators_282 {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if (num == null || num.length() == 0) return res;
+        backtracking(res, "", num, target, 0, 0, 0);
+        return res;
+    }
+
+    public void backtracking(List<String> res, String path, String num, int target, int pos, long eval, long multed) {
+        if (pos == num.length()) {
+            if (target == eval) { //check overflow
+                res.add(path);
+            }
+            return;
+        }
+        for (int i = pos; i < num.length(); i++) {
+            if (i != pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                backtracking(res, path + cur, num, target, i + 1, cur, cur);
+            } else {
+                backtracking(res, path + "+" + cur, num, target, i + 1, eval + cur, cur);
+                backtracking(res, path + "-" + cur, num, target, i + 1, eval - cur, -cur);
+                backtracking(res, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur);
+            }
+        }
+    }
+}
+
