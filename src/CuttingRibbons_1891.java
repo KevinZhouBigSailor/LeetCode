3e472c39223c3f855e06a2b5b1048b72d3e4d55e
KevinZhouBigSailor <kevinzhou1990@hotmail.com>
2022-02-11 09:17:21 -0800

    add

---
+++ src/CuttingRibbons_1891.java
@@ -0,0 +1,24 @@
+public class CuttingRibbons_1891 {
+    // https://leetcode.com/problems/cutting-ribbons/discuss/1263437/Java-Simple-Binary-Search-+-Explanation/1003412
+    public int maxLength(int[] ribbons, int k) {
+        int max = Integer.MIN_VALUE;
+        for (int len : ribbons)
+            max = Math.max(max, len);
+
+        int n = ribbons.length, low = 1, high = max, mid, count;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+
+            count = 0;
+            for (int length : ribbons)
+                count += length / mid;
+
+            if (count < k)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return high > 0 ? high : 0;
+    }
+}
---
+++ src/Facebook/GroupShiftedStrings_249.java
@@ -0,0 +1,33 @@
+package Facebook;
+
+import java.util.*;
+
+/**
+ * Created by zzhou on 1/26/2018.
+ */
+public class GroupShiftedStrings_249 {
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strings) {
+            int offset = str.charAt(0) - 'a';
+            String key = "";
+            for (int i = 0; i < str.length(); i++) {
+                char c = (char) (str.charAt(i) - offset);
+                if (c < 'a') c += 26;
+                key += c;
+            }
+            if (!map.containsKey(key)) {
+                List<String> list = new ArrayList<>();
+                map.put(key, list);
+            }
+            map.get(key).add(str);
+        }
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            Collections.sort(list);
+            res.add(list);
+        }
+        return res;
+    }
+}
